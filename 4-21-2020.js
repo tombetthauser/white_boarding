71554ceee164038827dff6df2861a8b630e1531d
tombetthauser <tombetthauser@gmail.com>
2020-04-22 10:41:34 -0700

    ProdArr refactored and finished

--- 4-21-2020.js
+++ 4-21-2020.js
@@ -1,46 +1,44 @@
-let prodarray = (inputarray) => {
-  if (inputarray.length < 2) return undefined;
-  // if (inputarray.length === 1 && typeof inputarray[0] != `number`) return undefined;
-  let totalproduct;
-  const returnarray = [];
+let prodArray = (inputArray) => {
+  if (inputArray.length < 2) return undefined;
 
-  const zeroHash = { index: null, count: 0 }
-  let zeroindex = null;
-  let zerocount = 0;
+  let totalProduct;
 
-  for (let i = 0; i < inputarray.length; i++) {
-    let element = inputarray[i];
+  const returnArr = [];
+  const zeroHash = { index: null, count: 0 };
+
+  for (let i = 0; i < inputArray.length; i++) {
+    let element = inputArray[i];
     if (element === 0) {
-      zeroindex = i;
-      zerocount += 1;
+      zeroHash.index = i;
+      zeroHash.count += 1;
     } else if (typeof element === `number`) {
-      totalproduct = totalproduct ? totalproduct * element : element;
+      totalProduct = totalProduct ? totalProduct * element : element;
     }
   }
 
-  for (let i = 0; i < inputarray.length; i++) {
-    let element = inputarray[i];
-    if (zerocount > 1) {
-      returnarray.push(0);
-    } else if (zerocount === 1 && i != zeroindex) {
-      returnarray.push(0);
-    } else if (zerocount === 1 && i === zeroindex) {
-      returnarray.push(totalproduct);
+  for (let i = 0; i < inputArray.length; i++) {
+    let element = inputArray[i];
+    if (zeroHash.count > 1) {
+      returnArr.push(0);
+    } else if (zeroHash.count === 1 && i != zeroHash.index) {
+      returnArr.push(0);
+    } else if (zeroHash.count === 1 && i === zeroHash.index) {
+      returnArr.push(totalProduct);
     } else if (typeof element != `number`) {
-      returnarray.push(totalproduct)
+      returnArr.push(totalProduct)
     } else {
-      returnarray.push(totalproduct / element);
+      returnArr.push(totalProduct / element);
     }
   }
 
-  return returnarray;
+  return returnArr;
 }
 
-console.log(prodarray([1, 2, 3])) // => [6, 3, 2]
-console.log(prodarray([2, 4, 0])) // => [0, 0, 8]
-console.log(prodarray([0, 3, 1, 2, 0])) // => [0, 0, 0, 0, 0]
-console.log(prodarray([-6, 2, -4])) // => [-8, 24, -12]
-console.log(prodarray([2])) // => undefined
-console.log(prodarray([`cat`])) // => undefined
-console.log(prodarray([])) // => undefined
-console.log(prodarray([1, 5, `cat`, 2, [1, 2, `dog`] ])) // => [10, 2, 10, 5, 10]
+console.log(prodArray([1, 2, 3])) // => [6, 3, 2]
+console.log(prodArray([2, 4, 0])) // => [0, 0, 8]
+console.log(prodArray([0, 3, 1, 2, 0])) // => [0, 0, 0, 0, 0]
+console.log(prodArray([-6, 2, -4])) // => [-8, 24, -12]
+console.log(prodArray([2])) // => undefined
+console.log(prodArray([`cat`])) // => undefined
+console.log(prodArray([])) // => undefined
+console.log(prodArray([1, 5, `cat`, 2, [1, 2, `dog`] ])) // => [10, 2, 10, 5, 10]
